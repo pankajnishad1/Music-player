6bd6f803388e3547e71474e3df9cdf8ed330e335
pankajnishad1 <pankajnishad024@gmail.com>
2026-02-08 17:53:47 +0530

    host fix issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,7 +6,9 @@ import { Provider } from 'react-redux'
 import { store } from './store/store'
 import React from 'react'
 createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <Provider store={store}>
     <App />
     </Provider>
+  </StrictMode>,
 )
